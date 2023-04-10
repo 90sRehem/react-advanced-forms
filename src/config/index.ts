7eeddb17bb0f42c4fb5ce0217e5252ab82a7f057
Jonathan <jonathan.rehem@hyperlocal.com.br>
2023-04-10 09:28:08 -0300

    add secret vars to env file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5mb
 export const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_KEY, SUPABASE_URL } from "../config";
 
-const supabaseUrl = "https://xyqqvxtfmycwzivrmnii.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5cXF2eHRmbXljd3ppdnJtbmlpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDgyNTc0MSwiZXhwIjoxOTk2NDAxNzQxfQ.wokBOXbKiBzTC35yYLndLFxX6-mHGseLVB0m3fVbnIU";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
